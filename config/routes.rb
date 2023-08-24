585213e59cfee8bb37fbd4eb086c751702c11354
komathi <komathi.laxmikanthan@weboniselab.com>
2023-08-24 12:24:22 +0530

    postcrud-testcase

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :comments
+  resources :authors
+  resources :posts
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
